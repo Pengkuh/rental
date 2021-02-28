843830293af26956893cc8a494147cdece94b2fd
Sultan Pengkuh <75963906+Pengkuh@users.noreply.github.com>
2021-02-28 20:10:21 +0700

    Update func.py

--- func.py
+++ func.py
@@ -1,12 +1,19 @@
 
+
 # Daftar Mobil dan Harganya
 merek = {
     "hb" : "Honda Brio",
-    "mx" : "Mitsubitsi Expander"
+    "mx" : "Mitsubitsi Expander",
+    "ys" : "Yamaha Supra",
+    "sc" : "Suzuki Carry",
+    "ta" : "Toyota Avanza"
 }
 harga = {
     "hb" : 200000,
-    "mx" : 250000
+    "mx" : 250000,
+    "ys" : 300000,
+    "sc" : 150000,
+    "ta" : 180000
 }
 
 
@@ -17,14 +24,20 @@ harga = {
 def menu ():
     print("\na. Rental Langsung\nb. Booking Mobil(MASIH PENGEMBANGAN)\nc. Data Client\n")
 
+def tabel ():
+    print("KODE NAMA")
+    print(merek)
+    print(" HARGA")
+    print(harga,"\n")
+
 # daftar client dafttar_client()
 def daftar_client(data_client):
     for res in data_client:
         print("--------------------------")
         print(f"Nama     : {res['nama']}")
-        print(f"Merek    : {res['merek']}")
+        print(f"Merek    : {res['jenis']}")
         print(f"Hari     : {res['hari']}")
-        print(f"Harga    : {res['harga']}")
+        print(f"Harga    : {res['pay']}")
         print(f"Total    : {res['total']}")
 
 # Rental Langsung rental_mobil()
@@ -35,26 +48,20 @@ def rental_mobil ():
     nama = input ("nama Lu      :   ")
     kode = input ("kode Mobil   :   ")
     hari = int(input ("Lama nya     :   "))
-    merek = merek[kode]
-    harga = harga[kode]
-    total = hari * harga
-
-    print("\n")
-    print("Nama Pemesan : ",nama)
-    print("Merek Mobil  : ",merek)
-    print("Harga Mobil  : ",harga)
-    print("Jumlah Hari  : ",hari)
-    print('Total Biaya  : Rp.',"{:,}".format(int(total)).replace(',','.'))
-
+    jenis = merek[kode]
+    pay = harga[kode]
+    total = hari * pay
 
     # Masuk Ke Log
     res = {
     "nama" : nama,
-    "merek" : merek,
+    "merek" : jenis,
     "hari"  : hari,
-    "harga" : harga,
+    "harga" : pay,
     "total" : total
     }
+
+    daftar_client(res)
 
     return res
 
